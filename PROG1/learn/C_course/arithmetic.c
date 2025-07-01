daedad0395b1c7b349b09bc409768e3d7d89e1df
TUF_garoa <raulvilela29@gmail.com>
2025-07-01 16:29:38 -0400

    learning C: operators.

---
+++ PROG1/learn/C_course/arithmetic.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+    // arithmetic operators = + - * / % ++ --
+
+    int x = 10;
+    int y = 3;
+    int z = 0;
+
+    //z = x + y;
+    //z = x - y;
+    //z = x * y;
+    //z = x / y; // CAUTION: the variable that is dividing the other and the one that will store the result, MUST BE FLOAT!!
+    //z = x % y; // get the reminder
+    //z++;
+    //z--;
+
+    //augmented assingment operators
+    // x+=3;
+    // x-=3;
+    // x*=2;
+    // x/=2;
+
+    printf("%d\n", z);
+
+}
