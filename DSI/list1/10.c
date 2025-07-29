01ef908746f120e1fb58692ab192ea494fe981d7
TUF_garoa <raulvilela29@gmail.com>
2025-07-29 18:49:41 -0400

    DSI lista exercicios: 10 soma de matrizes

---
+++ DSI/list1/10.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+    int matrixA[4][4], matrixB[4][4], i, j;
+
+    printf("Digite os valores de 2 matrizes 4x4, que eu farei a soma delas!!\n\n");
+
+    printf("%d\n", sizeof(matrixA)/4/4);
+
+    printf("Matrix A: \n");
+    for(i=0;i<sizeof(matrixA)/4/4; i++){
+        printf("\tInsira os valores da linha %d: \n", i+1);
+        for(j=0;j<sizeof(matrixA)/4/4;j++){
+            printf("\t\tValor %d: ", j+1);
+            scanf("%d", &matrixA[i][j]);
+        }
+    }
+
+    printf("Matrix B: \n");
+    for(i=0;i<sizeof(matrixB)/4/4; i++){
+        printf("\tInsira os valores da linha %d: \n", i+1);
+        for(j=0;j<sizeof(matrixB)/4/4;j++){
+            printf("\t\tValor %d: ", j+1);
+            scanf("%d", &matrixB[i][j]);
+        }
+    }
+
+    printf("A soma das duas matrizes eh:\n");
+    for(i=0;i<sizeof(matrixB)/4/4; i++){
+        printf("|");
+        for(j=0;j<sizeof(matrixB)/4/4;j++){
+            if(j==3){
+                printf("%d", matrixA[i][j]+matrixB[i][j]);
+            }
+            printf("%d\t", matrixA[i][j]+matrixB[i][j]);
+        }
+        printf("|\n");
+    }
+
+    return 0;
+}
