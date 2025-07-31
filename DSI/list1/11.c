14d8bcfc19834b83b726f5278bf7c1bd211054ac
R-Vilela <raulvilela29@gmail.com>
2025-07-31 12:11:17 -0400

    DS lista exercicios: 11 encontrar substring em uma string(versao completa)

--- DSI/list1/11.c
+++ DSI/list1/11.c
@@ -1,29 +1,50 @@
 #include <stdio.h>
-#include <string.h>
+
+int strleng(char str[]){
+    int i;
+
+    for(i=0;str[i]!='\0';i++);
+
+    return i;
+}
 
 int main(){
-    int i, size;
+    int i, j, aux=-1;
     char str[40];
+    char substr[] = "casa";
 
     printf("Ola! Escreva uma frase com ate 40 caracteres.\n");
     printf("\bATENCAO!! Eh proibido escrever \"casa\". EU SABEREI!\n");
 
     printf("Escreva sua frase: ");
-    fgets(str, sizeof(str), stdin);
-    str[strlen(str) -1] = '\0';
+    //fgets(str, sizeof(str), stdin);
+    //str[strleng(str) -1] = '\0';
+    //scanf("%s", str);
+    scanf("%[^\n]", str);
 
-    size = strlen(str)-3;
-
-    if(size>=4){
-        for(i=0;i<size&&!(str[i]=='c'&&str[i+1]=='a'&&str[i+2]=='s'&&str[i+3]=='a');i++);
+    if(strleng(str)>=4){
+        for(i=0;i<strleng(str);i++){
+            if(aux==-1){
+                for(j=0;j<strleng(substr);j++){
+                    if(str[i]==substr[j]){
+                        aux=i;
+                        i++;
+                        continue;
+                    }
+                    break;
+                }
+            }else{
+                break;
+            }
+        };
     }
 
     printf("\nVoce escreveu: %s\nE...\n\n", str);
 
-    if(i==size){
-        printf("Nao tem \"casa\" escrito!\n");
-    }else{
+    if(i>=0){
         printf("Tem \"casa\" escrito!\n MALDITU SEJAA!!!!");
+    }else{
+        printf("Nao tem \"casa\" escrito!\n");
     }
 
     return 0;
