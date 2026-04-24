0c6c72eb7b9059efa611d535d78cad826bf987a7
Raul Vilela <raulvilela29@gmail.com>
2026-04-23 21:22:12 -0400

    Main para testes

--- monitoria/POO/atv2/Main.java
+++ monitoria/POO/atv2/Main.java
@@ -1,7 +1,21 @@
-import model.Produto;
+import model.ProdutoAlimenticio;
+import model.ProdutoEletronico;
+import model.ProdutoVestuario;
 
-public class Main{
-    public static void main(String[] args) {
-        //Produto p = new Produto("FCD1928","teclado", 12, 12.45);
-    }
+import java.time.LocalDate;
+
+public class Main {
+  public static void main(String[] args) {
+    ProdutoAlimenticio pa = new ProdutoAlimenticio("ALI1928", "Cenoura", 12, 7.45, LocalDate.now());
+    ProdutoEletronico pe1 = new ProdutoEletronico("ELE1947", "Teclado", 12, 12.45);
+    ProdutoEletronico pe2 = new ProdutoEletronico("ELE1926", "Mouse", 9, 10.95, 3);
+    ProdutoEletronico pe3 = new ProdutoEletronico("ELE1926", "Monitor", 6, 32.95, 7);
+    ProdutoVestuario pv = new ProdutoVestuario("VES1954", "Vestido", 13, 27.75, "M", "Algodao");
+
+    System.out.println(pa.exibirDetalhes());
+    // System.out.println(pe1.exibirDetalhes());
+    // System.out.println(pe2.exibirDetalhes());
+    System.out.println(pe3.exibirDetalhes());
+    System.out.println(pv.exibirDetalhes());
+  }
 }
